0972e86f93db691e62f3bdb5d089d1bf4a6e510c
ViktorEgai <egai.viktor94@gmail.com>
2021-05-21 16:39:27 +0300

    create: added  booking modal window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSwiper = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  const hotelSwiper = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
  
@@ -30,41 +31,34 @@ const reviewsSwiper = new Swiper('.reviews-slider', {
 
 $('.newsletter').parallax({imageSrc: 'img/newsletter-bg.jpg'});
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log('Эклик по кнопке');
-  document
-  .querySelector(".navbar-menu")
-  .classList.toggle("navbar-menu--visible");
+var menuButton = $(".menu-button");
+menuButton.on("click", function () {
+  $(".navbar-menu")
+  .toggleClass("navbar-menu--visible");
 
 });
-function send(event, php) {
-        console.log("Отправка запроса");
-        event.preventDefault ? event.preventDefault() : (event.returnValue = false);
-        var req = new XMLHttpRequest();
-        req.open("POST", php, true);
-        req.onload = function () {
-          if (req.status >= 200 && req.status < 400) {
-            json = JSON.parse(this.response); // Ебанный internet explorer 11
-            console.log(json);
 
-            // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
-            if (json.result == "success") {
-              // Если сообщение отправлено
-              alert("Сообщение отправлено");
-            } else {
-              // Если произошла ошибка
-              alert("Ошибка. Сообщение не отправлено");
-            }
-            // Если не удалось связаться с php файлом
-          } else {
-            alert("Ошибка сервера. Номер: " + req.status);
-          }
-        };
+var modalButton = $('[data-toggle=modal]');
+modalButton.on("click", openModal);
+function openModal() {
+  var modal = $(".modal");
+  modal.addClass("modal--visible");  
+  };
 
-        // Если не удалось отправить запрос. Стоит блок на хостинге
-        req.onerror = function () {
-          alert("Ошибка отправки запроса");
-        };
-        req.send(new FormData(event.target));
-      };
+var closeModalButton = $(".modal__close");
+closeModalButton.on("click", closeModal);
+function closeModal(event) {
+  event.preventDefault();
+  var modal = $(".modal");
+  modal.removeClass("modal--visible");  
+  };
+
+$(document).keyup(function(e) {
+	if (e.key === "Escape" || e.keyCode === 27) {
+		e.preventDefault();
+  var modal = $(".modal");
+  modal.removeClass("modal--visible")
+	}
+});
+
+});
